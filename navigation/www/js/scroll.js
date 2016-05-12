18a1a7338e13dab6992b3a15ee3f5672532b883e
marwanebou <marwanebou@gmail.com>
2016-05-12 11:59:07 +0200

    versio0.2.2

--- navigation/www/js/scroll.js
+++ navigation/www/js/scroll.js
@@ -1,20 +1,6 @@
-/*!
- * liScroll 1.0
- * Examples and documentation at: 
- * http://www.gcmingati.net/wordpress/wp-content/lab/jquery/newsticker/jq-liscroll/scrollanimate.html
- * 2007-2010 Gian Carlo Mingati
- * Version: 1.0.2.1 (22-APRIL-2011)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- * Requires:
- * jQuery v1.2.x or later
- * 
- */
-jQuery.fn.liScroll = function(settings) 
-{
+jQuery.fn.liScroll = function(settings) {
 		settings = jQuery.extend({
-		travelocity: 0.02
+		travelocity: 0.07
 		}, settings);		
 		return this.each(function(){
 				var $strip = jQuery(this);
