d261546fd04ffe31e09802267f373a90cc823eb8
marwanebou <marwanebou@gmail.com>
2016-05-19 09:59:35 +0200

    saugarde

--- navigation/platforms/android/src/com/sp/qrcode/MainActivity.java
+++ navigation/platforms/android/src/com/sp/qrcode/MainActivity.java
@@ -21,12 +21,11 @@ package com.sp.qrcode;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
-
 public class MainActivity extends CordovaActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
-    {
+    {   
         super.onCreate(savedInstanceState);
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
--- navigation/www/js/position.js
+++ navigation/www/js/position.js
@@ -1,3 +1,23 @@
+javascript:window.history.forward(1);
+localStorage.clear();
+var url1= "http://ws-ifsproject2016.rhcloud.com/server.php/Affiche_restaurant";
+var pl = new SOAPClientParameters();
+SOAPClient.invoke(url1,"Affiche_restaurant", pl, false, restau_callBack);
+function restau_callBack(a)
+{ 
+  localStorage.setItem("list", a.restaurant);
+  var mytabex=a.restaurant.split("#");
+  var i=0;
+  var id_restaurant="";
+  while (i < ((mytabex.length)-6)) 
+	  { 
+		id_restaurant="restaurant_"+mytabex[i+4]+"="+mytabex[i];
+		$("#zoneresto").append("<a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'><div class=col-md-12 col-sm-12><div class=blog-post><div class=blog-thumb> <img id=img src='http://ws-ifsproject2016.rhcloud.com/img/"+mytabex[i+4]+".png'> </div> <div class=blog-content><div class=content-show><a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'>"+mytabex[i]+"</a></div><div style='display: none;' class=content-hide><p></p></div></div></div> </div></a>");
+		$("#recherche").append("<li><a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'>"+mytabex[i]+"</a></li>");
+		$("#recherche1").append("<li><a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'>"+mytabex[i]+"</a></li>");
+		i=i+6;
+	  }
+}
 function initialize() { 
 				$.mobile.loading( "show", {
 				text: "loading..",
@@ -27,7 +47,7 @@ function initialize() {
                 while (i <= ((tab.length)-6))
 				{			 
 			           var marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(tab[i+3],tab[i+2]),
+                        position: new google.maps.LatLng(tab[i+2],tab[i+1]),
                         map: map,
                         title: 'Click Me ' + i
                     });
@@ -62,38 +82,33 @@ function initialize() {
 					$("#map-canvas").append("<center><button onclick='return initialize();'>Actualiser</button></center>");					
 					} 	
 				}
-				navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});			
-			}			
-google.maps.event.addDomListener(window, 'load', initialize); 	
-document.addEventListener("offline", onOfline, false);
-function onOfline() 
+navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});			
+}			
+google.maps.event.addDomListener(window, 'load', initialize);
+/*
+function affiche()
 {
-alert("Verifier votre connexion internet");
-}	
-if (localStorage.getItem("username") != null)
-{
-window.location.href='dashboard.html';
-}
-else 
-{
-javascript:window.history.forward(1);
-localStorage.clear();
 var url1= "http://ws-ifsproject2016.rhcloud.com/server.php/Affiche_restaurant";
 var pl = new SOAPClientParameters();
-SOAPClient.invoke(url1,"Affiche_restaurant", pl, false, restau_callBack);
-function restau_callBack(a)
-{    
-  localStorage.setItem("list", a.restaurant);
-  var mytabex=a.restaurant.split("#");
-  var i=0;
-  var id_restaurant="";
-  while (i < ((mytabex.length))-6) 
-	  { 
-		id_restaurant="restaurant_"+mytabex[i+5]+"="+mytabex[i];
-		$("#zoneresto").append("<a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'><div class=col-md-12 col-sm-12><div class=blog-post><div class=blog-thumb> <img src='data:image/jpeg;base64,"+mytabex[i+1]+"'> </div> <div class=blog-content><div class=content-show><a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'>"+mytabex[i]+"</a></div><div style='display: none;' class=content-hide><p></p></div></div></div> </div></a>");
-		$("#recherche").append("<li><a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'>"+mytabex[i]+"</a></li>");
-		$("#recherche1").append("<li><a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'>"+mytabex[i]+"</a></li>");
-		i=i+6;
-	  }
+SOAPClient.invoke(url1,"Affiche_restaurant", pl, false, restau);
+function restau(a)
+	{ 
+	  var list= localStorage.getItem("list");
+	  if (list != a.restaurant)
+	  {
+	  localStorage.setItem("list", a.restaurant);
+	  var mytabex=a.restaurant.split("#");
+	  var i=0;
+	  var id_restaurant = "";
+	  $(".bhim").empty();
+		while (i < ((mytabex.length)-6) )
+			{	
+				id_restaurant="restaurant_"+mytabex[i+4]+"="+mytabex[i];
+				$("#zoneresto").append("<a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'><div class=col-md-12 col-sm-12><div class=blog-post><div class=blog-thumb> <img id=img src='http://ws-ifsproject2016.rhcloud.com/img/"+mytabex[i+4]+".png'> </div> <div class=blog-content><div class=content-show><a onclick=window.location.href='accueil.html?restaurant="+id_restaurant+"'>"+mytabex[i]+"</a></div><div style='display: none;' class=content-hide><p></p></div></div></div> </div></a>");
+				i=i+6;
+			}
+	 }
+	}
 }
-}
+setInterval("affiche()",5000);
+*/
